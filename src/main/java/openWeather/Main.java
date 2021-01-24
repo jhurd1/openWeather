00679683fb9eb664b29bccde178bfb5c4bd94cda
Jamie Hurd <jaoshu@bitbucket.org>
2021-01-23 19:38:44 -0700

    back to bascis

--- src/main/java/openWeather/Main.java
+++ src/main/java/openWeather/Main.java
@@ -2,28 +2,36 @@ package openWeather;
 
 import com.google.gson.Gson;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main
 {
-    private static String city;
+    /*private static String city;
     private static final String url = "https://api.openweathermap.org/data/2.5/weather";
     private static final String key = "39d90d5929e83265f2f1669ea3752ed3";
     private static final String charset = "UTF-8";
+    private static int id = this.id;
+    private static String name = this.name;
+    static Map<String, Float> main = new HashMap<String, Float>;
+*/
 
-    public static void main (String[] args) throws UnsupportedEncodingException {
-        System.out.print("Enter city: ");
+    public static void main (String[] args) throws IOException {
+        System.out.print("Enter city name: ");
         Scanner scanner = new Scanner(System.in);
-        city = scanner.next();
+        WeatherConditions w = new WeatherConditions();
+        String name = w.getName();
+        name = scanner.next();
+        Gson gson = new Gson();
+        w = gson.fromJson(w.weatherUnderground(), WeatherConditions.class); //deserialized string to object
 
-        try
+
+       /* try w.getId() + w.getName();
         {
             // ?q=London,uk&APPID=
             String query = String.format("?q=%s&apiKey=%s",
@@ -51,6 +59,6 @@ public class Main
         catch (Exception e)
         {
             e.printStackTrace();
-        }
+        }*/
     }
 }
--- src/main/java/openWeather/WeatherConditions.java
+++ src/main/java/openWeather/WeatherConditions.java
@@ -1,16 +1,9 @@
 package openWeather;
 
 import com.google.gson.Gson;
-import com.google.gson.annotations.SerializedName;
+import com.google.gson.JsonElement;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.URLEncoder;
-import java.util.HashMap;
 import java.util.Map;
 
 /*****************************************
@@ -26,56 +19,69 @@ public class WeatherConditions
      ******************************************/
     private int id;
     private String name;
-
-    Map<String, Float> measures;
-    @SerializedName("main")
-    private String url = "api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=39d90d5929e83265f2f1669ea3752ed3";
+    Map<String, Float> main;
+    //@SerializedName("main")
+    /*private String url = "api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=39d90d5929e83265f2f1669ea3752ed3";
     private String charset = "UTF-8";
     private String param1 = "";
     private String param2 = "";
     private String query = String.format("param1=%s&param2=%s",
             URLEncoder.encode(param1, charset),
-            URLEncoder.encode(param1, charset));
+            URLEncoder.encode(param1, charset));*/
 
-    Gson g = new Gson();
+    public String weatherUnderground()
+    {
+            Gson g = new Gson();
+            String gsonOutput = g.toJson(main); //serialized the string
+        return gsonOutput;
+    }
+
 
     /*******************************************
      * CONSTRUCTOR
      ******************************************/
-    WeatherConditions(int id, String name, int main) throws UnsupportedEncodingException
+
+    WeatherConditions()
+    {
+
+    }
+    WeatherConditions(int id, String name, Map<String, Float> main) throws UnsupportedEncodingException
     {
         this.id = id;
         this.name = name;
+        this.main = main;
     }
 
     /*******************************************
      * ACCESSORS
      ******************************************/
-    public int getId() {
+    public int getId()
+    {
         return id;
     }
 
-    public String getName() {
-        return name;
+    public String getName()
+    {
+        return this.name;
     }
 
-    public Gson getG() {
-        return g;
-    }
-
-
-    public void setId(int id) {
+    public void setId(int id)
+    {
         this.id = id;
+    }
+
+    public Map<String, Float> getMain() {
+        return main;
     }
 
     /*******************************************
     * HTTPGET
     * TEST HTTP GET
     ******************************************/
-    public void httpGet() throws IOException
+    /*public void httpGet() throws IOException
     {
         URLConnection connection = new URL(url + "?" + query).openConnection();
         connection.setRequestProperty("Accept-Charset", charset);
         InputStream response = connection.getInputStream();
-    }
+    }*/
 }
